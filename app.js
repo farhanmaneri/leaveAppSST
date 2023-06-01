848a144d667c57e45370776afd6b50c35cd83963
farhanmaneri <farhanmaneri@gmail.com>
2023-06-01 16:06:00 +0500

    create a leave sanction app for SST staff

--- app.js
+++ app.js
@@ -9,34 +9,41 @@ const print = () => {
   document.getElementById('myForm').addEventListener('submit', function(event) {
 event.preventDefault()
   
-    var gender = document.getElementById('gender').value;
+    // var gender = document.getElementById('gender').value;
     var name = document.getElementById('name').value.toUpperCase();
+    document.getElementById("nameCell").textContent = name;
+    var cnic = document.getElementById('cnic').value.toUpperCase();
+    document.getElementById("cnicCell").textContent = cnic;
+    
     var designation = document.getElementById('designation').value;
-    var current_station = document.getElementById('current_station').value.toUpperCase();
-    var to_station = document.getElementById('to_station').value.toUpperCase();
-    var remarks = document.getElementById('remarks').value;
-    var old_station = document.getElementById('old_station').value;
-    var new_station = document.getElementById('new_station').value;
+    document.getElementById("scaleCell").textContent = designation;
+
+    // var current_station = document.getElementById('current_station').value.toUpperCase();
+    // var to_station = document.getElementById('to_station').value.toUpperCase();
+    // var remarks = document.getElementById('remarks').value.toUpperCase();
+    // var old_station = document.getElementById('old_station').value;
+    // var new_station = document.getElementById('new_station').value;
    
 
-    var table = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
-    var rowIndex = table.rows.length + 1; // Get the new rowIndex
-    var newRow = table.insertRow();
+    // var table = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
+    // var rowIndex = table.rows.length + 1; // Get the new rowIndex
+    // var newRow = table.insertRow();
     
 
   
 
-    var serialCell = newRow.insertCell(0)
-    var nameCell = newRow.insertCell(1);
-    var current_stationCell = newRow.insertCell(2);
-    var to_stationCell = newRow.insertCell(3);
-    var remarksCell = newRow.insertCell(4);
+    // var serialCell = newRow.insertCell(0)
+    // var nameCell = newRow.insertCell(1);
+    // var current_stationCell = newRow.insertCell(2);
+    // // var to_stationCell = newRow.insertCell(3);
+    // // var remarksCell = newRow.insertCell(4);
 
-    serialCell.innerHTML = rowIndex
-    nameCell.innerHTML = gender +" "+name  +","+" "+ designation
-    current_stationCell.innerHTML = old_station +" "+current_station;
-    to_stationCell.innerHTML = new_station +" "+to_station;
-    remarksCell.innerHTML = remarks;
+    // // serialCell.innerHTML = rowIndex
+    // // nameCell.innerHTML = gender +" "+ name  +","+" "+ designation
+    // // nameCell.innerHTML = "Name"
+    // current_stationCell.innerHTML = old_station +" "+current_station;
+    // to_stationCell.innerHTML = new_station +" "+to_station;
+    // remarksCell.innerHTML = remarks;
 
     document.getElementById('myForm').reset();
   
