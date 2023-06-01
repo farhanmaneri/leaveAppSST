ffa0fc5a98c0314dac9ae9a31537653ed7bd14ae
farhanmaneri <farhanmaneri@gmail.com>
2023-06-01 12:16:00 -0700

    changes made

--- app.js
+++ app.js
@@ -10,21 +10,46 @@ const print = () => {
 event.preventDefault()
   
     var gender = document.getElementById('gender').value;
+
     var name = document.getElementById('name').value.toUpperCase();
     document.getElementById("nameCell").textContent = gender+" " +name;
+    
     var cnic = document.getElementById('cnic').value
     document.getElementById("cnicCell").textContent = cnic;
     
     var designation = document.getElementById('designation').value;
     document.getElementById("scaleCell").textContent = designation;
-
-    // var current_station = document.getElementById('current_station').value.toUpperCase();
-    // var to_station = document.getElementById('to_station').value.toUpperCase();
-    // var remarks = document.getElementById('remarks').value.toUpperCase();
-    // var old_station = document.getElementById('old_station').value;
-    // var new_station = document.getElementById('new_station').value;
+    
+    var leave_from = document.getElementById('leave_from').value;
+    var leave_upto = document.getElementById('leave_upto').value;
+    document.getElementById("leaveCell").textContent = leave_from+" " +"to"+""+ leave_upto;
    
+    var leaveTpye = document.getElementById('leaveTpye').value;
+    document.getElementById("leaveTpyeCell").textContent = leaveTpye;    
+    
+    var leaveTpye = document.getElementById('leaveTpye').value;
+    document.getElementById("leaveTpyeCell").textContent = leaveTpye;    
+    
+    var country_abroad = document.getElementById('country_abroad').value;
+    document.getElementById("countryCell").textContent = country_abroad;    
+    
+    var leave_date = document.getElementById('leave_date').value;
+    document.getElementById("exactDateCell").textContent = leave_date;    
+    
+    var reason = document.getElementById('reason').value;
+    document.getElementById("reasonCell").textContent = reason;    
+    
+    var personal_number = document.getElementById('personal_number').value;
+    document.getElementById("personalNoCell").textContent = personal_number;
 
+    var previous_leave = document.getElementById('previous_leave').value;
+    document.getElementById("previouLeaveCell").textContent = previous_leave;    
+    
+    var service_length = document.getElementById('service_length').value;
+    document.getElementById("serviceLengthCell").textContent = service_length;    
+    
+    
+    
     // var table = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
     // var rowIndex = table.rows.length + 1; // Get the new rowIndex
     // var newRow = table.insertRow();
